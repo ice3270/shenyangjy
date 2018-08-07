9d2dcfae9f408390d7d4661ae9ed64d5b4bdf86d
LaoA <ice_wan@msn.cn>
2018-08-07 23:20:47 +0800

    更新数据库文件

--- syjy.sql
+++ syjy.sql
@@ -1,22 +1,24 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : MySQL
-Source Server Version : 50719
-Source Host           : localhost:3306
-Source Database       : syjy
+ Source Server         : MySQL-ROOT
+ Source Server Type    : MySQL
+ Source Server Version : 50639
+ Source Host           : localhost
+ Source Database       : syjy
 
-Target Server Type    : MYSQL
-Target Server Version : 50719
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50639
+ File Encoding         : utf-8
 
-Date: 2018-08-06 15:36:42
+ Date: 08/07/2018 23:18:31 PM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for banner
+--  Table structure for `banner`
 -- ----------------------------
 DROP TABLE IF EXISTS `banner`;
 CREATE TABLE `banner` (
@@ -28,14 +30,14 @@ CREATE TABLE `banner` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of banner
+--  Records of `banner`
 -- ----------------------------
-INSERT INTO `banner` VALUES ('1', '展示图一 测试标题', '展示图一 测试描述 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '1533109949');
-INSERT INTO `banner` VALUES ('2', '展示图二 测试标题', '展示图二 测试描述 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '1533109949');
-INSERT INTO `banner` VALUES ('3', '展示图三 测试标题', '展示图三 测试描述 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '1533109949');
+BEGIN;
+INSERT INTO `banner` VALUES ('1', '展示图一 测试标题1234567890-', '展示图一 测试描述 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '1533642072'), ('2', '展示图二 测试标题', '展示图二 测试描述 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '1533642072'), ('3', '展示图三 测试标题', '展示图三 测试描述 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '1533642072');
+COMMIT;
 
 -- ----------------------------
--- Table structure for company_index
+--  Table structure for `company_index`
 -- ----------------------------
 DROP TABLE IF EXISTS `company_index`;
 CREATE TABLE `company_index` (
@@ -46,12 +48,32 @@ CREATE TABLE `company_index` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of company_index
+--  Records of `company_index`
 -- ----------------------------
+BEGIN;
 INSERT INTO `company_index` VALUES ('1', '企业板块测试标题文字', '企业板块正文测试文字 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字');
+COMMIT;
 
 -- ----------------------------
--- Table structure for serve_index
+--  Table structure for `company_show_index`
+-- ----------------------------
+DROP TABLE IF EXISTS `company_show_index`;
+CREATE TABLE `company_show_index` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_show_title` varchar(255) NOT NULL,
+  `company_show_text` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `company_show_index`
+-- ----------------------------
+BEGIN;
+INSERT INTO `company_show_index` VALUES ('1', '测试标题一', '测试描述文字一'), ('2', '测试标题二', '测试描述文字二'), ('3', '测试标题三', '测试描述文字三');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `serve_index`
 -- ----------------------------
 DROP TABLE IF EXISTS `serve_index`;
 CREATE TABLE `serve_index` (
@@ -63,15 +85,14 @@ CREATE TABLE `serve_index` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of serve_index
+--  Records of `serve_index`
 -- ----------------------------
-INSERT INTO `serve_index` VALUES ('1', 'balance-scale', '服务于司法', '竭诚服务于司法 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字');
-INSERT INTO `serve_index` VALUES ('2', 'flag', '服务于公安', '竭诚服务于公安 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字');
-INSERT INTO `serve_index` VALUES ('3', 'asl-interpreting', '服务于公司', '竭诚服务于公司 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字');
-INSERT INTO `serve_index` VALUES ('4', 'users', '服务于百姓', '竭诚服务于百姓 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字');
+BEGIN;
+INSERT INTO `serve_index` VALUES ('1', 'balance-scale', '服务于司法', '竭诚服务于司法 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字'), ('2', 'flag', '服务于公安', '竭诚服务于公安 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字'), ('3', 'asl-interpreting', '服务于公司', '竭诚服务于公司 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字'), ('4', 'users', '服务于百姓', '竭诚服务于百姓 测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字');
+COMMIT;
 
 -- ----------------------------
--- Table structure for slogan_index
+--  Table structure for `slogan_index`
 -- ----------------------------
 DROP TABLE IF EXISTS `slogan_index`;
 CREATE TABLE `slogan_index` (
@@ -83,9 +104,10 @@ CREATE TABLE `slogan_index` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of slogan_index
+--  Records of `slogan_index`
 -- ----------------------------
-INSERT INTO `slogan_index` VALUES ('1', '（一）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（一）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务一');
-INSERT INTO `slogan_index` VALUES ('2', '（二）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（二）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务二');
-INSERT INTO `slogan_index` VALUES ('3', '（三）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（三）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务三');
-INSERT INTO `slogan_index` VALUES ('4', '（四）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（四）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务四');
+BEGIN;
+INSERT INTO `slogan_index` VALUES ('1', '（一）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（一）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务一'), ('2', '（二）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（二）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务二'), ('3', '（三）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（三）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务三'), ('4', '（四）服务宗旨标题测试文字：最强最强我最强，最棒最棒我最棒', '（四）服务宗旨详情测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字', '服务四');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
